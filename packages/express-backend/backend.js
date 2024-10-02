24191b5dec88eec03581494194a83c97f9ce0428
nidhiraviprasad <nidhi.raviprasad@gmail.com>
2024-10-02 14:03:19 -0700

    added deletion by id functinality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -96,6 +96,24 @@ const findUserById = (id) =>
     res.send();
   });
 
+const deleteUserById = (id) => {
+  users["users_list"] = users["users_list"].filter(
+    (user) => user["id"] !== id
+  );
+};
+
+app.delete("/users/:id", (req, res) => {
+    const id = req.params.id;
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } 
+    else {
+      deleteUserById(id);
+      res.send();
+    }
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
