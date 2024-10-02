6aaa077a675906317e63ed26eff045f4e0021a5e
nidhiraviprasad <nidhi.raviprasad@gmail.com>
2024-10-02 12:25:46 -0700

    added get and add user functionality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -58,6 +58,30 @@ app.get("/users", (req, res) => {
   }
 });
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
+
+  const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
